51c7c380c1075eaf466cb0f494a315e8ab1bf2cf
jakkie <jakubkielbasiewicz@gmail.com>
2021-08-07 19:55:20 +0000

    Initial Kucoin Connector

---
+++ balance/exchanges/kucoin/kucoin_authenticator.py
@@ -0,0 +1,23 @@
+class KucoinAuthenticator:
+    def __init__(self, api_key: str, api_secret: str, api_passphrase: str):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.api_passphrase = api_passphrase
+
+    def authenticate(self):
+        url = 'https://api.kucoin.com/api/v1/accounts'
+        now = int(time.time() * 1000)
+        str_to_sign = str(now) + 'GET' + '/api/v1/accounts'
+        signature = base64.b64encode(
+            hmac.new(api_secret.encode('utf-8'), str_to_sign.encode('utf-8'), hashlib.sha256).digest())
+        passphrase = base64.b64encode(hmac.new(api_secret.encode('utf-8'), api_passphrase.encode('utf-8'), hashlib.sha256).digest())
+        headers = {
+            "KC-API-SIGN": signature,
+            "KC-API-TIMESTAMP": str(now),
+            "KC-API-KEY": api_key,
+            "KC-API-PASSPHRASE": passphrase,
+            "KC-API-KEY-VERSION": 2
+        }
+        response = requests.request('get', url, headers=headers)
+        print(response.status_code)
+        return response.json()
---
+++ balance/exchanges/kucoin/kucoin_connector.py
@@ -0,0 +1,9 @@
+from kucoin_authenticator import KucoinAuthenticator
+
+class KucoinConnector():
+    def __init__(self, auth: KucoinAuthenticator):
+
+
+    def get_balance(self):
+        
+        
