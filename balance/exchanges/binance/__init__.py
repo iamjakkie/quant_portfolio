f8fff00b627ef6b3039a5963bcd86c0d7dd74bb2
jakkie <jakubkielbasiewicz@gmail.com>
2022-01-24 16:25:13 +0100

    asyncio binance

--- balance/exchanges/binance/__init__.py
+++ balance/exchanges/binance/__init__.py
@@ -2,3 +2,10 @@ import asyncio
 
 from binance_authenticator import BinanceAuthenticator
 from binance_connector import BinanceConnector
+
+async def main():
+    pass
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
