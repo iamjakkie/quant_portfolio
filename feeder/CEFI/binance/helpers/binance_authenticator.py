d56e1a458fc7c62c32154d245530942b18acb1cd
jakkie <jakubkielbasiewicz@gmail.com>
2024-07-15 16:03:40 +0200

    binance authenticator cleanup

--- feeder/CEFI/binance/helpers/binance_authenticator.py
+++ feeder/CEFI/binance/helpers/binance_authenticator.py
@@ -1,3 +1,11 @@
 from binance import Client 
-
 from FEEDER.BASE_MODEL.authenticator import Authenticator
+
+class BinanceAuthenticator(Authenticator):
+    def __init__(self, api_key: str, secret_key: str):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.client = Client
+
+    def authenticate(self):
+        return Client(self.api_key, self.secret_key)
